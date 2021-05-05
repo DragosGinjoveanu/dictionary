725a838fade84f39210ebf654c4f6360c3db48cf
Ginjoveanu Dragos <82390006+DragosGinjoveanu@users.noreply.github.com>
2021-05-05 16:50:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-      var wordArray = [];
-      //this function adds the word
-      function addWord() {
-        text = document.getElementById('word').value;
-        wordArray.push(text);
-        $('#list').append(`
-          <div class="card">
-            <div class="card-body">
-              <p class="text-center">` + text + `</p>  
-            </div>
-          </div>  
-        `); 
-      }
-      // this function searches if the given word is already in the list
-      function searchWord() {
-        text = document.getElementById('word').value;
-        if (wordArray.indexOf(text) != -1) {
-          swal({
-            text: "The word is in the list",
-            icon: "success",
-          });
-        } else {
-          swal({
-            text: "The word is NOT in the list",
-            icon: "error",
-          });
-        }
-      }
-      // this function clears the list
-      function clearList() {
-        location.reload();
-      }
+var wordArray = [];
+//this function adds the word
+function addWord() {
+  document.getElementById("status").innerHTML = "";
+  text = document.getElementById('word').value;
+  wordArray.push(text);
+  $('#list').append(`
+    <div class="card">
+      <div class="card-body">
+        <p class="text-center">` + text + `</p>  
+      </div>
+    </div>  
+  `); 
+}
+// this function searches if the given word is already in the list
+function searchWord() {
+  text = document.getElementById('word').value;
+  if (wordArray.indexOf(text) != -1) {
+    document.getElementById("status").style.color = "green";
+    document.getElementById("status").innerHTML = "The word is in the list";
+  } else {
+    document.getElementById("status").style.color = "red";
+    document.getElementById("status").innerHTML = "The word is not in the list";
+  }
+}
+// this function clears the list
+function clearList() {
+  location.reload();
+}
